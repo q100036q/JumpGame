a7121d2f68bde8323d0c788bb76ea7859fa0bd1b
夏洛伊de风 <dhr19941012@163.com>
2018-02-01 23:33:07 +0800

    add some big change
    1.add a swith button to control game start and close.
    2.add help instructions to tips.
    3.add more Scale change to Player.
    4.add touch control for Player.
    5.change Star.js code structure for ccNodePool,it's easy to get new item or put for destory.

--- jumpGame/assets/script/JavaScript/Game.js
+++ jumpGame/assets/script/JavaScript/Game.js
@@ -1,13 +1,3 @@
-// Learn cc.Class:
-//  - [Chinese] http://www.cocos.com/docs/creator/scripting/class.html
-//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/class/index.html
-// Learn Attribute:
-//  - [Chinese] http://www.cocos.com/docs/creator/scripting/reference/attributes.html
-//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/reference/attributes/index.html
-// Learn life-cycle callbacks:
-//  - [Chinese] http://www.cocos.com/docs/creator/scripting/life-cycle-callbacks.html
-//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/life-cycle-callbacks/index.html
-
 cc.Class({
     extends: cc.Component,
 
@@ -38,6 +28,30 @@ cc.Class({
             default: null,
             url: cc.AudioClip,
         },
+
+        btnNode:{
+            default: null,
+            type: cc.Node,
+        },
+
+        gameOverNode:{
+            default: null,
+            type: cc.Node,
+        },
+
+        controlLabel:{
+            default: null,
+            type: cc.Label,
+        },
+
+        keyboardHint:{
+            default:'',
+            multiline: true,
+        },
+        touchHint:{
+            default: '',
+            multiline: true,
+        },
     },
 
     // LIFE-CYCLE CALLBACKS:
@@ -47,24 +61,49 @@ cc.Class({
 
         this.timer = 0;
         this.starDuration = 0;
-        this.newStarFunc();
-        this.score = 0;
+
+        this.isRunning = false;
+        var hintText = cc.sys.isMobile ? this.touchHint:this.keyboardHint;
+        this.controlLabel.string = hintText;
+
+        this.starPool = new cc.NodePool('Star');
     },
     newStarFunc: function(){
-        var newStar = cc.instantiate(this.starPrefab);
+        var newStar = null;
+        if (this.starPool.size()>0){
+            newStar = this.starPool.get(this);
+        }else{
+            newStar = cc.instantiate(this.starPrefab);
+        }
         this.node.addChild(newStar);
         newStar.setPosition(this.getNewStarPosition());
-        newStar.getComponent('Star').game = this;
+        newStar.getComponent('Star').init(this);
 
+        this.startTimer();
+        this.currentStar = newStar;
+    },
+    despawnStar(star){
+        this.starPool.put(star);
+        this.newStarFunc();
+    },
+    startTimer: function(){
         this.starDuration = this.minStarDuration + cc.random0To1() * (this.maxStarDuration - this.minStarDuration);
         this.timer = 0;
     },
-
     getNewStarPosition: function(){
+        if(!this.currentStar){
+            //记录上一次星星的位置，为了让星星每次显示的位置中央对齐分布
+            this.currentStarX = this.node.width/2 * cc.randomMinus1To1();
+        }
         var randomX = 0;
         var randomY = this.groundY + cc.random0To1() * this.player.getComponent('Player').jumpHeight+50;
         var maxX = this.node.width/2;
-        randomX = cc.randomMinus1To1()*maxX;
+        if (this.currentStarX >= 0){
+            randomX = - cc.random0To1() * maxX;
+        }else{
+            randomX = cc.random0To1()*maxX;
+        }
+        this.currentStarX = randomX;
         return cc.p(randomX,randomY);
     },
     gainScore: function(){
@@ -72,15 +111,27 @@ cc.Class({
         this.scoreDisplay.string = 'Score:' + this.score.toString();
         cc.audioEngine.playEffect(this.scoreAduio,false);
     },
-    start () {
-
+    onStartGame: function() {
+        this.resetScore();
+        this.isRunning = true;
+        this.btnNode.setPositionX(3000);
+        this.gameOverNode.active = false;
+        this.player.getComponent('Player').startMoveAt(cc.p(0,this.groundY));
+        this.newStarFunc();
+    },
+    resetScore: function(){
+        this.score = 0;
+        this.scoreDisplay.string = 'Score:' + this.score.toString();
     },
     gameOver: function(){
-        this.player.stopAllActions();
-        //this.player.removeKeyBoardEvent();
-        cc.director.loadScene('JumpGame');
+        this.player.getComponent('Player').stopMove();
+        this.gameOverNode.active = true;
+        this.isRunning = false;
+        this.btnNode.setPositionX(0);
+        this.currentStar.destroy();
     },
     update: function(dt) {
+        if (!this.isRunning) return;
         if(this.timer > this.starDuration)
         {
             this.gameOver();
