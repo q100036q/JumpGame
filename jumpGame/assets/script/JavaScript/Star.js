f126616911bf02c7ef781f2fe04b81f4494ff9b3
q100036q <184212460@qq.com>
2018-02-02 11:24:16 +0800

    revert complete project
    Solve the problems caused by misoperation

--- jumpGame/assets/script/JavaScript/Star.js
+++ jumpGame/assets/script/JavaScript/Star.js
@@ -1,32 +1,32 @@
-// Learn cc.Class:
-//  - [Chinese] http://www.cocos.com/docs/creator/scripting/class.html
-//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/class/index.html
-// Learn Attribute:
-//  - [Chinese] http://www.cocos.com/docs/creator/scripting/reference/attributes.html
-//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/reference/attributes/index.html
-// Learn life-cycle callbacks:
-//  - [Chinese] http://www.cocos.com/docs/creator/scripting/life-cycle-callbacks.html
-//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/life-cycle-callbacks/index.html
-
 cc.Class({
     extends: cc.Component,
 
     properties: {
         pickRadius: 0,
+        game:{
+            default: null,
+            serializable: false,
+        },
     },
 
     // LIFE-CYCLE CALLBACKS:
 
-    // onLoad () {},
+    onLoad () {},
+    init: function(game){
+        this.game = game;
+        this.node.opacity = 255;
+    },
+    reuse(game){
+        this.init(game);
+    },
     getPlayerDistance:function(){
-        var playerPos = this.game.player.getPosition();
+        var playerPos = this.game.player.getComponent('Player').getCenterPos();
         var dist = cc.pDistance(this.node.position,playerPos);
         return dist;
     },
     onPicked: function(){
-        this.game.newStarFunc();
         this.game.gainScore();
-        this.node.destroy();
+        this.game.despawnStar(this.node);
     },
 
     start () {
